6c40f2bffc967bb60e46e04825d4812425865169
ilai <ilai.aloni@gmail.com>
2023-07-21 17:08:35 -0700

    Company web template

--- Company_web.py
+++ Company_web.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas
 
+st.set_page_config(layout="wide")
 content = """Zim dreams of greatness. Unfortunately, though, he's hopelessly inept as a space invader. Desperate to be rid of the annoying Zim, 
             his planet's leaders send him on a mission to infiltrate Earth, providing him with leftover, cobbled-together equipment. 
             To their consternation, Zim succeeds in setting up a base on Earth and infiltrating human culture,
@@ -10,6 +12,25 @@ st.header("The Best Company")
 st.write(content)
 
 st.subheader("Our Team")
-col1, empty1 , col2, empty2, col3 = st.columns([1.5, .5, 1.5, .5, 1.5])
+col1, empty1, col2, empty2, col3 = st.columns([1.5, 1, 1.5, 1, 1.5])
+
+df = pandas.read_csv("data_C.csv", sep=',')
 
 
+with col1:
+    for index, row in df[:4].iterrows():
+        st.header(f'{row["first name"].title()} {row["last name"].title()}')
+        st.image("company_images/" + row["image"])
+        st.write(row["role"])
+
+with col2:
+    for index, row in df[4:8].iterrows():
+        st.header(f'{row["first name"].title()} {row["last name"].title()}')
+        st.image("company_images/" + row["image"])
+        st.write(row["role"])
+
+with col3:
+    for index, row in df[8:].iterrows():
+        st.header(f'{row["first name"].title()} {row["last name"].title()}')
+        st.image("company_images/" + row["image"])
+        st.write(row["role"])
